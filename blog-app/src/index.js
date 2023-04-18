96f0e04215c04c8dabbdb3d08b0df815d0c9ad83
zahra-mgh <zahramoshfghi@yahoo.com>
2023-04-18 19:03:33 +0430

    Launch the client

--- blog-app/src/index.js
+++ blog-app/src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {ApolloClient , ApolloProvider , InMemoryCache} from "@apollo/client" ;
+const client = new ApolloClient({
+  uri:"https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clg6kvlpx0vwi01um2u1wgwlx/master" ,
+  cache:new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
