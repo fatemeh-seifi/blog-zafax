0c23765c853ae0ddc3075685a4e01f28a8e344f8
fatemeh seifi <fatemehseifi630@gmail.com>
2023-04-19 02:31:18 +0430

    make searchIcon and input and logo

--- blog-app/src/components/layout/Header.js
+++ blog-app/src/components/layout/Header.js
@@ -1,7 +1,72 @@
 import React from 'react'
+// import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import {AppBar} from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import Box from '@mui/material/Box';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(10),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 5.5, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(7)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 1.5),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
 
 export default function Header() {
+  
   return (
-    <div>Header</div>
+   <AppBar position="sticky">
+      <Toolbar>
+        <Typography component="div" variant="h4">
+            زافاکس
+        </Typography>
+        <Search>
+          <SearchIconWrapper>
+              <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+              placeholder="عنوان مقاله را وارد کنید ..."
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+      </Toolbar>
+
+   </AppBar>
   )
 }
